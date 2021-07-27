1bd629ffc50a3e8d1a3ebbfa1baf9ad0d3fd007b
DavisAsano <dfasano@iastate.edu>
2021-07-26 17:24:45 -0700

    organizing

--- sl-deploy.py
+++ sl-deploy.py
@@ -8,6 +8,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 import os
 import numpy as np
+from scipy import stats
+import zipfile
 
 
 #####
@@ -15,6 +17,10 @@ import numpy as np
 ## dfp > m0/m1 --- plot all correlations for modes
 ## mo/m1 > m0k0/m1k0 --- plot all correlations for keys in modes
 #####
+
+with zipfile.ZipFile()
+
+
 
 # Read in all CSV files
 path = r'C:\Users\dasan\Desktop\Repo\Music_Data_Asano' #Read multiple CSV by pattern matching
@@ -34,60 +40,66 @@ df["Year Released"] = df["Year Released"].dt.year
 ### Popularity filter ###
 # Filter dataframe to only songs with a popularity score 80+
 df_popular = df[df['popularity'] >= 80]
-dfpcorr = df_popular.corr()
+dfpcorr = df_popular
 # Filter popular dataframe by key
-pk0 = df_popular[df_popular['key'] == 0].corr()
-pk1 = df_popular[df_popular['key'] == 1].corr()
-pk2 = df_popular[df_popular['key'] == 2].corr()
-pk3 = df_popular[df_popular['key'] == 3].corr()
-pk4 = df_popular[df_popular['key'] == 4].corr()
-pk5 = df_popular[df_popular['key'] == 5].corr()
-pk6 = df_popular[df_popular['key'] == 6].corr()
-pk7 = df_popular[df_popular['key'] == 7].corr()
-pk8 = df_popular[df_popular['key'] == 8].corr()
-pk9 = df_popular[df_popular['key'] == 9].corr()
-pk10 = df_popular[df_popular['key'] == 10].corr()
-pk11 = df_popular[df_popular['key'] == 11].corr()
+pk0 = df_popular[df_popular['key'] == 0]
+pk1 = df_popular[df_popular['key'] == 1]
+pk2 = df_popular[df_popular['key'] == 2]
+pk3 = df_popular[df_popular['key'] == 3]
+pk4 = df_popular[df_popular['key'] == 4]
+pk5 = df_popular[df_popular['key'] == 5]
+pk6 = df_popular[df_popular['key'] == 6]
+pk7 = df_popular[df_popular['key'] == 7]
+pk8 = df_popular[df_popular['key'] == 8]
+pk9 = df_popular[df_popular['key'] == 9]
+pk10 = df_popular[df_popular['key'] == 10]
+pk11 = df_popular[df_popular['key'] == 11]
 
 ### Mode and Key filters ###
 # Filter popular dataframe to mode 0 
 m0 = df_popular[df_popular['mode'] == 0]
-m0corr = m0.corr()
+m0corr = m0
 #Filter mode0 dataframe by key
-m0k0 = m0[m0['key'] == 0].corr()
-m0k1 = m0[m0['key'] == 1].corr()
-m0k2 = m0[m0['key'] == 2].corr()
-m0k3 = m0[m0['key'] == 3].corr()
-m0k4 = m0[m0['key'] == 4].corr()
-m0k5 = m0[m0['key'] == 5].corr()
-m0k6 = m0[m0['key'] == 6].corr()
-m0k7 = m0[m0['key'] == 7].corr()
-m0k8 = m0[m0['key'] == 8].corr()
-m0k9 = m0[m0['key'] == 9].corr()
-m0k10 = m0[m0['key'] == 10].corr()
-m0k11 = m0[m0['key'] == 11].corr()
+m0k0 = m0[m0['key'] == 0]
+m0k1 = m0[m0['key'] == 1]
+m0k2 = m0[m0['key'] == 2]
+m0k3 = m0[m0['key'] == 3]
+m0k4 = m0[m0['key'] == 4]
+m0k5 = m0[m0['key'] == 5]
+m0k6 = m0[m0['key'] == 6]
+m0k7 = m0[m0['key'] == 7]
+m0k8 = m0[m0['key'] == 8]
+m0k9 = m0[m0['key'] == 9]
+m0k10 = m0[m0['key'] == 10]
+m0k11 = m0[m0['key'] == 11]
 # Filter popular dataframe to mode 1
 m1 = df_popular[df_popular['mode'] == 1]
-m1corr = m1.corr()
+m1corr = m1
 # Filter mode1 dataframe by key
-m1k0 = m1[m1['key'] == 0].corr()
-m1k1 = m1[m1['key'] == 1].corr()
-m1k2 = m1[m1['key'] == 2].corr()
-m1k3 = m1[m1['key'] == 3].corr()
-m1k4 = m1[m1['key'] == 4].corr()
-m1k5 = m1[m1['key'] == 5].corr()
-m1k6 = m1[m1['key'] == 6].corr()
-m1k7 = m1[m1['key'] == 7].corr()
-m1k8 = m1[m1['key'] == 8].corr()
-m1k9 = m1[m1['key'] == 9].corr()
-m1k10 = m1[m1['key'] == 10].corr()
-m1k11 = m1[m1['key'] == 11].corr()
+m1k0 = m1[m1['key'] == 0]
+m1k1 = m1[m1['key'] == 1]
+m1k2 = m1[m1['key'] == 2]
+m1k3 = m1[m1['key'] == 3]
+m1k4 = m1[m1['key'] == 4]
+m1k5 = m1[m1['key'] == 5]
+m1k6 = m1[m1['key'] == 6]
+m1k7 = m1[m1['key'] == 7]
+m1k8 = m1[m1['key'] == 8]
+m1k9 = m1[m1['key'] == 9]
+m1k10 = m1[m1['key'] == 10]
+m1k11 = m1[m1['key'] == 11]
 
+col1,col2 = st.beta_columns(2)
+st.title('Relationships in Music Data (1960-2020)')
+st.subheader("This app lets you explore different correlations in popular songs (popularity rating of 80+).")
 st.write("""
-# A Music Data Exploration App
-
-This app lets you explore different correlations in popular songs over the years
+* Only relationships with a significant Correlation Coefficent may be selected
+** r > 0.30  r < -0.30 **
+* The steeper the line the stronger the correlation
 """)
+###
+
+
 
 st.sidebar.header('User Input Parameters')
 
@@ -397,7 +409,7 @@ if mode == 'All':
                     st.pyplot()
             if X == 'energy':
                 Y = st.sidebar.selectbox('Y',('acousticness',''))
-                if Y == 'v':
+                if Y == 'acousticness':
                     sns.regplot(x=pk4['energy'],y=pk4['acousticness'])
                     st.pyplot()
             if X == 'loudness':
